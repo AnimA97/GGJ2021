b7249bfddae2a8b429fa7e0de5b3c008c89dc193
Samuel Noble <samuel.noble@patagonia-e.com>
2021-01-30 13:06:03 -0300

    [Camera][Animations] Working on the camera following up and animations

--- Assets/GAME/Scripts/Camera/FollowBackCamera.cs
+++ Assets/GAME/Scripts/Camera/FollowBackCamera.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowBackCamera : MonoBehaviour
 {
+
+    public PlayerMovementController moveCtrl;
+
+    public float cameraSpeed;
+    public Vector3 offset;
+    public float speedThreshold;
+    public float distanceThreshold;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Mathf.Abs(moveCtrl.GetSpeed().x) < speedThreshold) return;
+
+        Vector3 currOffset = new Vector3(Mathf.Sign(moveCtrl.GetSpeed().x) * offset.x, offset.y, offset.z);
+        Vector3 desiredPosition = moveCtrl.transform.position + currOffset;
+
+        Debug.Log(Mathf.Abs(transform.position.x - desiredPosition.x));
+        if (Mathf.Abs(moveCtrl.transform.position.x - desiredPosition.x) < distanceThreshold)
+            transform.position = moveCtrl.transform.position + currOffset;
+
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, cameraSpeed);
+        transform.position = smoothedPosition;
     }
+
 }
---
+++ Assets/GAME/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+
+    //Configuraciones
+    public float speed = 2.5f;
+    public float jumpForce = 7.5f;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius;
+
+    //Componentes
+    private Rigidbody2D _rigidbody;
+
+    //Auxiliares
+    private Vector2 _movement;
+    private bool _isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontalVelocity = _movement.normalized.x * speed;
+        _rigidbody.velocity = new Vector2(horizontalVelocity, _rigidbody.velocity.y);
+    }
+
+    private void Movement()
+    {
+        //Horizontal
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        _movement = new Vector2(horizontalInput, 0f);
+
+        //Vertical
+        _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (Input.GetButtonDown("Jump") && _isGrounded == true)
+        {
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public Vector2 GetSpeed()
+    {
+        return _rigidbody.velocity;
+    }
+
+}
