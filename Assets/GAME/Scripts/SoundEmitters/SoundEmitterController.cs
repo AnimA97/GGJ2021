171109b5e7279211ab4e6071e7b779401c62bc73
Samuel Noble <samuel.noble@patagonia-e.com>
2021-01-31 12:47:21 -0300

    [SCENARIO] Added sounds/music and creating background.

---
+++ Assets/GAME/Scripts/SoundEmitters/SoundEmitterController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitterController : MonoBehaviour
+{
+
+    public AudioSource _audio;
+    public AudioClip[] audios;
+    public float timerToShout = 12f;
+
+    private float timeToShout = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToShout += Time.deltaTime;
+        if (timeToShout > timerToShout)
+        {
+            timeToShout = 0f;
+            _audio.PlayOneShot(audios[Random.Range(0, audios.Length)]);
+            Debug.Log("Sonido!");
+        }
+    }
+}
