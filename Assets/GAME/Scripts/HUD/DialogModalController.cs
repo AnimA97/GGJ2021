671673ddde653bdae5fe44670f052e6a7e295b35
Samuel Noble <samuel.noble@patagonia-e.com>
2021-01-30 23:03:13 -0300

    [ANIMATIONS][HUD] Error message of police | fixes on sniff and animations | Pause added.

---
+++ Assets/GAME/Scripts/HUD/DialogModalController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogModalController : MonoBehaviour
+{
+
+    public DialogueHandler dialogeHandler;
+
+    public void ShowMessage()
+    {
+        gameObject.SetActive(true);
+        if (dialogeHandler == null) return;
+        dialogeHandler.AnimateDialogueBox("¡Perro malo!\n¡Busca, busca!");
+    }
+
+    public void HideMessage()
+    {
+        if (dialogeHandler == null || dialogeHandler.messageFinished)
+        {
+            gameObject.SetActive(false);
+            if (GameSystem.instance != null) GameSystem.instance.state = GameState.PLAYING;
+        }
+    }
+
+}
---
+++ Assets/GAME/Scripts/System/GameSystem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState
+{
+    PLAYING, PAUSE
+}
+
+public class GameSystem : MonoBehaviour
+{
+
+    public static GameSystem instance;
+
+    public GameObject player;
+    public DialogModalController dialogModal;
+    public GameState state;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            dialogModal.HideMessage();
+            DontDestroyOnLoad(this.gameObject);
+        } else
+        {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dialogModal.enabled && Input.anyKey)
+        {
+            dialogModal.HideMessage();
+        }
+    }
+
+    public void ShowPoliceShoutingMessageModal()
+    {
+        state = GameState.PAUSE;
+        dialogModal.ShowMessage();
+    }
+
+    internal bool isPaused()
+    {
+        return state.Equals(GameState.PAUSE);
+    }
+}
